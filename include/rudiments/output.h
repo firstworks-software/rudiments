61a6624e7e4f20478cf26c93d8c6a34a8455fa20
David Muse <david.muse@firstworks.com>
2018-02-26 11:41:53 -0500

    added write with timeout to output class

--- include/rudiments/output.h
+++ include/rudiments/output.h
@@ -22,6 +22,33 @@ class RUDIMENTS_DLLSPEC output {
 		virtual	ssize_t	write(uint64_t number)=0;
 		virtual	ssize_t	write(float number)=0;
 		virtual	ssize_t	write(double number)=0;
+
+		virtual	ssize_t	write(const unsigned char *string, size_t size,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(const char *string,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(const char *string, size_t size,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(char character,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(int16_t number,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(int32_t number,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(int64_t number,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(unsigned char character,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(uint16_t number,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(uint32_t number,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(uint64_t number,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(float number,
+						int32_t sec, int32_t usec);
+		virtual	ssize_t	write(double number,
+						int32_t sec, int32_t usec);
 };
 
 #endif
---
+++ src/output.cpp
@@ -0,0 +1,58 @@
+// Copyright (c) 2018 David Muse
+// See the COPYING file for more information
+
+#include <rudiments/output.h>
+
+ssize_t output::write(const unsigned char *string, size_t size,
+						int32_t sec, int32_t usec) {
+	return write(string,size);
+}
+
+ssize_t output::write(const char *string, int32_t sec, int32_t usec) {
+	return write(string);
+}
+
+ssize_t output::write(const char *string, size_t size,
+						int32_t sec, int32_t usec) {
+	return write(string,size);
+}
+
+ssize_t output::write(char character, int32_t sec, int32_t usec) {
+	return write(character);
+}
+
+ssize_t output::write(int16_t number, int32_t sec, int32_t usec) {
+	return write(number);
+}
+
+ssize_t output::write(int32_t number, int32_t sec, int32_t usec) {
+	return write(number);
+}
+
+ssize_t output::write(int64_t number, int32_t sec, int32_t usec) {
+	return write(number);
+}
+
+ssize_t output::write(unsigned char character, int32_t sec, int32_t usec) {
+	return write(character);
+}
+
+ssize_t output::write(uint16_t number, int32_t sec, int32_t usec) {
+	return write(number);
+}
+
+ssize_t output::write(uint32_t number, int32_t sec, int32_t usec) {
+	return write(number);
+}
+
+ssize_t output::write(uint64_t number, int32_t sec, int32_t usec) {
+	return write(number);
+}
+
+ssize_t output::write(float number, int32_t sec, int32_t usec) {
+	return write(number);
+}
+
+ssize_t output::write(double number, int32_t sec, int32_t usec) {
+	return write(number);
+}
