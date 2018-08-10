30e2d0d41747c7fdd28d05ceb7bdb662b5d5de86
David Muse <david.muse@firstworks.com>
2018-08-09 21:40:45 -0400

    added private sys.h

---
+++ include/rudiments/private/sys.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2018 David Muse
+// See the COPYING file for more information.
+
+	static	bool	mAdvise(unsigned char *start,
+					size_t length,
+					int32_t advice);
+
+	static	bool	mLockAll(int32_t flags);
