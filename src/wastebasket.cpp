4ad865b28cfe88bf3daa94ae45e05366b74cc1d9
David Muse <dmuse@spectrum.firstworks.com>
2021-06-30 16:14:26 -0400

    updates

---
+++ src/wastebasket.cpp
@@ -0,0 +1,59 @@
+// Copyright (c) 1999-2018 David Muse
+// See the COPYING file for more information.
+
+#include <rudiments/wastebasket.h>
+#include <rudiments/linkedlist.h>
+
+class wastebasketprivate {
+	friend class wastebasket;
+	private:
+		linkedlist<object *>		*_objects;
+		linkedlist<collection *>	*_deletecollections;
+		linkedlist<collection *>	*_arraydeletecollections;
+};
+
+wastebasket::wastebasket() : object() {
+	pvt=new wastebasketprivate;
+	pvt->_objects=NULL;
+	pvt->_deletecollections=NULL;
+	pvt->_arraydeletecollections=NULL;
+}
+
+wastebasket::~wastebasket() {
+	empty();
+	delete pvt;
+}
+
+void wastebasket::attachObject(object *o) {
+	if (!pvt->_objects) {
+		pvt->_objects=new linkedlist<object *>();
+	}
+	pvt->_objects->append(o);
+}
+
+void wastebasket::attachDeleteCollection(collection *c) {
+	if (!pvt->_deletecollections) {
+		pvt->_deletecollections=new linkedlist<collection *>();
+	}
+	pvt->_deletecollections->append(c);
+}
+
+void wastebasket::empty() {
+	if (pvt->_objects) {
+		pvt->_objects->clearAndDelete();
+		delete pvt->_objects;
+		pvt->_objects=NULL;
+	}
+	if (pvt->_deletecollections) {
+		for (listnode<collection *> *node=
+				pvt->_deletecollections->getFirst();
+				node; node=node->getNext()) {
+#ifdef NODE_DELETE
+			node->getValue()->clearAndDelete();
+#endif
+		}
+		pvt->_deletecollections->clearAndDelete();
+		delete pvt->_deletecollections;
+		pvt->_deletecollections=NULL;
+	}
+}
