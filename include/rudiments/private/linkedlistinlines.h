0115c3c9edb2477577b111f59603a7df86069c87
David Muse <david.muse@firstworks.com>
2021-11-29 15:16:00 -0500

    removed assignment to suppress unused variable warnings

--- include/rudiments/private/linkedlistinlines.h
+++ include/rudiments/private/linkedlistinlines.h
@@ -22,8 +22,7 @@ linkedlist<valuetype>::linkedlist() : listcollection<valuetype>() {
 	// code to be included and the symbols to be defined.  It's called
 	// here, after the return so that it never actually gets executed.
 	// The old compiler doesn't complain about that.
-	linkedlistnode<valuetype>	*a=
-		(linkedlistnode<valuetype> *)getFirst();
+	(linkedlistnode<valuetype> *)getFirst();
 #endif
 }
 
