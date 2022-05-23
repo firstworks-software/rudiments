03dc7e7399ecc04ef9543b60c09ab566c1bc2239
David Muse <david.muse@firstworks.com>
2022-05-23 17:41:50 -0400

    removed debug

--- test/csvdom.cpp
+++ test/csvdom.cpp
@@ -41,7 +41,6 @@ int main() {
 
 	csvdom	c;
 
-#if 0
 	stdoutput.printf("normal...\n");
 	c.parseString(normal);
 	c.writeFile("normal.csv",permissions::evalPermString("rw-r--r--"));
@@ -655,7 +654,6 @@ int main() {
 		!charstring::compare(c.getField(9,3),"vald") &&
 		!charstring::compare(c.getField(9,4),"vale");
 	test("carry all down",success);
-#endif
 
 	stringbuffer	longdata;
 	file		f;
