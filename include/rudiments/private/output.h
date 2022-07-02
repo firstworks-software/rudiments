745e7583311126fdf023f00e0a899cd78a51aa2a
David Muse <david.muse@firstworks.com>
2022-07-01 20:47:05 -0400

    added missing files

---
+++ include/rudiments/private/output.h
@@ -0,0 +1,7 @@
+// Copyright (c) 1999-2018 David Muse
+// See the COPYING file for more information.
+
+		virtual	ssize_t	printfDelegate(const char *format,
+							va_list *argp)=0;
+		virtual	ssize_t	printfDelegate(const wchar_t *format,
+							va_list *argp)=0;
