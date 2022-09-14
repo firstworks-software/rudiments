364192e80ce0399b981c503b4168e758ff0f4398
David Muse <david.muse@firstworks.com>
2022-09-14 14:02:39 -0400

    removed getRawData methods

---
+++ include/rudiments/private/crypt.h
@@ -0,0 +1,8 @@
+// Copyright (c) 1999-2018 David Muse
+// See the COPYING file for more information.
+
+		uint32_t		getKeySize();
+		const unsigned char	*getDecryptedData();
+
+	private:
+		cryptprivate	*pvt;
