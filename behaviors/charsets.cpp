6a712690941d6db04e5e9474bc5a7a10af91d618
David Muse <david.muse@firstworks.com>
2022-11-29 14:33:23 -0500

    added behaviors tests

---
+++ behaviors/charsets.cpp
@@ -0,0 +1,119 @@
+#include <rudiments/wcharstring.h>
+#include <rudiments/charstring.h>
+#include <rudiments/stringbuffer.h>
+#include <rudiments/stdio.h>
+
+#include <wchar.h>
+#include <stdio.h>
+#include <locale.h>
+
+#include "../test/test.cpp"
+
+int main(int argc, char **argv) {
+
+	// wchar_t size on this platform
+	header("wchar_t size");
+	stdoutput.printf("  sizeof(wchar_t): %d\n",sizeof(wchar_t));
+	stdoutput.printf("\n");
+
+	// define some locales
+	const char *locales[]={
+		"","POSIX","en_US.ISO-8859-1","en_US.UTF-8",NULL
+	};
+
+	// define some text strings
+	const wchar_t	*hellotxt=L"Hello!";
+	const wchar_t	hellohex[]={
+		0x00000048, 0x00000065, 0x0000006c, 0x0000006c, 
+		0x0000006f, 0x00000021, 0x00000000
+	};
+	const wchar_t	*chairetetxt=L"Χαίρετε!";
+	const wchar_t	chairetehex[]={
+		0x000003a7, 0x000003b1, 0x000003af, 0x000003c1, 
+		0x000003b5, 0x000003c4, 0x000003b5, 0x00000021, 
+		0x00000000
+	};
+	const wchar_t	*privettxt=L"Привет!";
+	const wchar_t	privethex[]={
+		0x0000041f, 0x00000440, 0x00000438, 0x00000432,
+		0x00000435, 0x00000442, 0x00000021, 0x00000000
+	};
+	const wchar_t	*texts[]={
+		hellotxt,
+		hellohex,
+		chairetetxt,
+		chairetehex,
+		privettxt,
+		privethex,
+		NULL
+	};
+	const char	*descriptions[]={
+		"\"Hello!\" in english (UCS-2/4 encoded text)",
+		"\"Hello!\" in english (UCS-2/4 hex)",
+		"\"Chairete!\" in greek (UCS-2/4 encoded text)",
+		"\"Chairete!\" in greek (UCS-2/4 hex)",
+		"\"Privet!\" in cyrillic (UCS-2/4 encoded text)",
+		"\"Privet!\" in cyrillic (UCS-2/4 hex)",
+		NULL
+	};
+
+	// iterate over locales
+	for (const char **locale=locales; *locale; locale++) {
+
+		// set locale
+		if (!charstring::isNullOrEmpty(*locale)) {
+			setlocale(LC_CTYPE,*locale);
+			header(*locale);
+		} else {
+			header("initial locale");
+		}
+
+		// display locale
+		stdoutput.printf("  requested locale: %s\n",*locale);
+		stdoutput.printf("  actual locale:    %s\n",
+						setlocale(LC_CTYPE,NULL));
+		stdoutput.printf("\n");
+
+
+		// iterate over text strings
+		uint16_t	counter=0;
+		const wchar_t	**text=texts;
+		const char	**description=descriptions;
+		while (*text && *description) {
+
+			// display description of the text
+			stdoutput.printf("%s\n",*description);
+
+			// display the text as hex
+			stdoutput.printf("  hex:\n");
+			uint16_t	hexcounter=0;
+			const wchar_t *t=*text;
+			for (; *t; t++) {
+				stdoutput.printf("0x%08x, ",*t);
+				hexcounter++;
+				if (!(hexcounter%4)) {
+					stdoutput.printf("\n");
+				}
+			}
+			stdoutput.printf("0x%08x",*t);
+			stdoutput.printf("\n");
+
+			// display the text as text, 3 different ways
+			stdoutput.printf("  write:     ");
+			stdoutput.write(*text);
+			stdoutput.printf("\n");
+			stdoutput.printf("  vfprintf:  ");
+			stdoutput.printf("%ls",*text);
+			stdoutput.printf("\n");
+			stdoutput.printf("  vfwprintf: ");
+			stdoutput.printf(L"%ls",*text);
+			stdoutput.printf("\n");
+			stdoutput.printf("\n");
+
+			// move on
+			counter++;
+			text++;
+			description++;
+		}
+	}
+}
---
+++ behaviors/vswprintf.cpp
@@ -0,0 +1,28 @@
+#include <rudiments/stdio.h>
+#include <rudiments/wcharstring.h>
+
+#include <stdio.h>
+#include <wchar.h>
+
+#include "../test/test.cpp"
+
+int myprintf(wchar_t *buffer, size_t length, const wchar_t *format, ...) {
+	va_list	args;
+	va_start(args,format);
+	size_t	result=vswprintf(buffer,length,format,args);
+	va_end(args);
+	return result;
+}
+
+int main() {
+
+	header("vswprintf");
+
+	stdoutput.printf("too small of a buffer:\n");
+	wchar_t	buf[2];
+	int	result=myprintf(buf,2,L"hello",NULL);
+	stdoutput.printf("  vswprintf(buf,2,L\"hello\",NULL);\n");
+	stdoutput.printf("  result: %d\n",result);
+	stdoutput.printf("  errno:  %d\n",error::getErrorNumber());
+	stdoutput.printf("\n");
+}
