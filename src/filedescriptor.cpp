e324641ebf2e15ac87251abde4ef5b498a473cfe
David Muse <david.muse@firstworks.com>
2022-12-08 01:21:35 -0500

    unixware tweaks

--- src/filedescriptor.cpp
+++ src/filedescriptor.cpp
@@ -215,7 +215,7 @@ extern ssize_t __xnet_sendmsg (int, const struct msghdr *, int);
 
 // on some platforms, we should use the stdin/out/err stream directly rather
 // than fdopen()ing a new stream for it
-#if defined(__SGICXX) || defined(__FreeBSD__)
+#if defined(__SGICXX) || defined(__FreeBSD__) || defined(_SCO_UW)
 	#define USESTDSTREAMS 1
 #endif
 
