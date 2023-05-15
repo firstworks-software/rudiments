c1fa6a0a1aef530545c47f1dbf9f5d7efd7ac70b
David Muse <david.muse@firstworks.com>
2023-05-15 17:34:29 -0400

    added test

--- test/locale.cpp
+++ test/locale.cpp
@@ -3,12 +3,97 @@
 
 #include <rudiments/locale.h>
 #include <rudiments/charstring.h>
+#include <rudiments/stringbuffer.h>
 #include <rudiments/stdio.h>
 #include "test.cpp"
+
+static const char *categories[]={
+	"LC_ALL",
+	"LC_COLLATE",
+	"LC_CTYPE",
+	"LC_MESSAGES",
+	"LC_MONETARY",
+	"LC_NUMERIC",
+	"LC_TIME",
+	"LC_ADDRESS",
+	"LC_IDENTIFICATION",
+	"LC_MEASUREMENT",
+	"LC_NAME",
+	"LC_PAPER",
+	"LC_TELEPHONE",
+	NULL
+};
+
+const char	*l="en_US.UTF-8";
 
 int main(int argc, const char **argv) {
 
 	header("locale");
+
+	// bail if not supported
+	if (!locale::isSupported()) {
+		stdoutput.printf("not supported\n");
+		return 0;
+	}
+
+	stringbuffer	title;
+
+	// default locale
+	stdoutput.printf("default locale (C):\n");
+	for (const char * const *cat=categories; *cat; cat++) {
+		if (!charstring::isInSet(*cat,locale::getCategories())) {
+			continue;
+		}
+		title.append("get ")->append(*cat)->append("=C");
+		test(title.getString(),
+			!charstring::compare(locale::getValue(*cat),"C"));
+		title.clear();
+	}
+
+	// set LC_ALL to another locale
+	stdoutput.printf("set LC_ALL to %s locale:\n",l);
+	title.append("set LC_ALL=")->append(l);
+	test(title.getString(),locale::setValue("LC_ALL",l));
+	title.clear();
+	for (const char * const *cat=categories; *cat; cat++) {
+		if (!charstring::isInSet(*cat,locale::getCategories())) {
+			continue;
+		}
+		title.append("get ")->append(*cat)->append("=")->append(l);
+		test(title.getString(),
+			!charstring::compare(locale::getValue(*cat),l));
+		title.clear();
+	}
+
+	// set individual categories back to C
+	stdoutput.printf("individuals back to C:\n");
+	for (const char * const *cat=categories+1; *cat; cat++) {
+
+		if (!charstring::isInSet(*cat,locale::getCategories())) {
+			continue;
+		}
+
+		title.append("set ")->append(*cat)->append("=C");
+		test(title.getString(),locale::setValue(*cat,"C"));
+		title.clear();
+
+		title.append("get ")->append(*cat)->append("=C");
+		test(title.getString(),
+			!charstring::compare(locale::getValue(*cat),"C"));
+		title.clear();
+
+		bool	notequalsc=true;
+		for (const char * const *ocat=cat+1; *ocat; ocat++) {
+			if (!charstring::isInSet(*cat,
+					locale::getCategories())) {
+				continue;
+			}
+			if (!charstring::compare(locale::getValue(*ocat),"C")) {
+				notequalsc=false;
+			}
+		}
+		test("others!=C",notequalsc);
+	}
 
 	return 0;
 }
