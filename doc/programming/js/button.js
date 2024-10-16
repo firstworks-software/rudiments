9db45da1bcb268aa6a27fb9de5d868386cd527f6
David Muse <david.muse@firstworks.com>
2024-10-16 14:32:30 -0400

    updates

--- doc/programming/js/button.js
+++ doc/programming/js/button.js
@@ -1,23 +1,3 @@
-function endsWith(str,ending) {
-	return str.indexOf(ending,str.length-ending.length)!==-1;
-}
-function contains(str,part) {
-	return str.includes(part);
-}
-
-function setActive(btn,img) {
-	document.getElementById("button-"+btn).style.backgroundImage="url(../images/"+img+")";
-}
-
 function setButton() {
-	if (endsWith(document.location.pathname,"index.html")) {
-		setActive("about","lightbluedenim.png");
-	} else if (endsWith(document.location.pathname,"documentation.html") ||
-			contains(document.location.pathname,"/programming/")) {
-		setActive("documentation","lightbluedenim.png");
-	} else if (endsWith(document.location.pathname,"download.html")) {
-		setActive("download","lightbluedenim.png");
-	} else if (endsWith(document.location.pathname,"license.html")) {
-		setActive("license","lightbluedenim.png");
-	}
+	document.getElementById("button-documentation").style.backgroundImage="url(../images/lightbluedenim.png)";
 }
